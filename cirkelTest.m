ba98bae39d6c217ae09a77d5acdfaa9917a4cd9c
GertjanEnHarald <spamport@hotmail.com>
2015-05-09 21:02:11 +0200

    cirkel ook

---
+++ cirkelTest.m
@@ -0,0 +1,15 @@
+close all;clc;clear all;
+
+x = rand(6,1);
+y = rand(6,1);
+[a,b,c,d,e,f] = cirkel(x,y);
+
+
+%[xAppr,yAppr] = rekenEllipsWaarde(a,b,c,d,e,f,x);
+fh = @(x,y) a*x^2 + 2*b*x*y + c*y^2 + d*x + e*y + f;
+ezplot(fh,[-1,1])
+%plot(real(xAppr),real(yAppr));
+hold on
+scatter(x,y);
+
+
