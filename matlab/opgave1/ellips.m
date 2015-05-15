cc2eabdc958f21b832120e07e123ecbc5f9a697b
GertjanEnHarald <spamport@hotmail.com>
2015-05-16 00:25:00 +0200

    Figuurtjes

--- matlab/opgave1/ellips.m
+++ matlab/opgave1/ellips.m
@@ -6,11 +6,8 @@ function [a,b,c,d,e,f] = ellips(x,y)
 
 columnOfOnes = ones(size(x,1),1);
 
-<<<<<<< HEAD
 A = [2*x.*y, y.*y - x.*x, x, y, columnOfOnes]; %A * coef = -x^2  % coef is vector met coefficienten (b,c,d,e,f)
-=======
 A = [2*x.*y, y.*y - x.*x, x, y, columnOfOnes];
->>>>>>> a6dae6ba7b45c7b3fc4fad1a3d480ac26f6782c2
 b = -x.*x;
 x = A\b;
 
--- matlab/opgave1/ellipsTest.m
+++ matlab/opgave1/ellipsTest.m
@@ -1,18 +1,15 @@
 close all;clc;clear all;
 
-x = rand(5,1);
-y = rand(5,1);
+x = rand(7,1);
+y = rand(7,1);
 [a,b,c,d,e,f] = ellips(x,y);
 
 
-<<<<<<< HEAD
 %[xAppr,yAppr] = rekenEllipsWaarde(a,b,c,d,e,f,x);
 fh = @(x,y) a*x^2 + 2*b*x*y + c*y^2 + d*x + e*y + f;
 ezplot(fh,[-2,2])
 %plot(real(xAppr),real(yAppr));
-=======
 error = tekenkegelsnede(a,b,c,d,e,f);
->>>>>>> a6dae6ba7b45c7b3fc4fad1a3d480ac26f6782c2
 hold on
 scatter(x,y);
 error
